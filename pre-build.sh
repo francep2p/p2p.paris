f11031bd2236c551733206c731e20c36f7071056
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2019-12-24 00:31:15 +0700

    reuse spreadsheet script

--- pre-build.sh
+++ pre-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-node ./scripts/generate-fest.js
-node ./scripts/generate-speakers.js
+node ./scripts/gsheetstojson.js '1z5GaDeDu2Ei_pCEFpFWSS53NZSmseBdDXuadUPh9qWE' 1547075518 'data/festival/events' 'festival'
+node ./scripts/gsheetstojson.js '12vIWrL2DnrfRlEZ23PFwv4sJO-rGtbD95WWsjirNqTk' 1242995252 'data/festival/speakers'
