db4928af4250c498178a39038f49157951598440
Cassius de Leeuwe <cadeleeuwe@gmail.com>
2019-12-28 16:16:38 +0700

    update script

--- scripts/generate-airtable-pages.js
+++ scripts/generate-airtable-pages.js
@@ -13,8 +13,93 @@ async function main() {
   const talks = await fetchTable('Talk');
   const speakers = await fetchTable('Speaker');
   const events = await fetchTable('Event');
+  const chapters = await fetchTable('Chapter');
 
-  console.log({ talks, speakers, events });
+  const translated = splitToMultiLanguage([
+    ...talks,
+    ...speakers,
+    ...events,
+    ...chapters
+  ]);
+
+  const en = joinRelations(translated.en);
+  const fr = joinRelations(translated.fr);
+
+  ['speaker', 'talk'].forEach(topic => {
+    generateMarkdownFiles(en.filter(item => item.from_table === topic));
+    generateMarkdownFiles(fr.filter(item => item.from_table === topic), 'fr');
+  });
+}
+
+function generateMarkdownFiles(items, langSuffix = '') {
+  const suffix = langSuffix ? `.${langSuffix}.md` : '.md';
+  items.forEach(item => {
+    const filepath = path.join(__dirname, '../content', item.file_path, `index${suffix}`);
+    const dirpath = path.dirname(filepath);
+
+    fs.mkdirSync(dirpath, { recursive: true });
+    fs.writeFileSync(filepath, JSON.stringify(item, null, 2));
+    console.log(`Created file: ${filepath}`);
+  })
+}
+
+function splitToMultiLanguage(items) {
+  let result = { en: [], fr: [] };
+
+  items.forEach(item => {
+    let translations = { en: {}, fr: {} };
+
+    Object.keys(item).forEach(key => {
+      const value = item[key];
+      if (isTranslated(key)) {
+        const lang = getLanguage(key);
+        translations[lang][removeLanguageKey(key)] = value;
+      } else {
+        translations.en[key] = value;
+        translations.fr[key] = value;
+      }
+    });
+
+    result.en.push(translations.en);
+    result.fr.push(translations.fr);
+  });
+
+  return result;
+}
+
+function joinRelations(items) {
+  const normalized = normalizeArray(items)
+
+  return items.map(item => {
+    let result = {};
+
+    Object.keys(item).forEach(key => {
+      let newValue;
+      const value = item[key];
+
+      if (value instanceof Array) {
+        newValue = value.map(v => {
+          return normalized[v]
+            ? relationObj(normalized[v])
+            : v;
+        });
+      } 
+      else if (normalized[value]) {
+        return relationObj(normalized[value]);
+      }
+      else {
+        newValue = value;
+      }
+
+      result[key] = newValue;
+    });
+  
+    return result;
+  })
+
+  function relationObj(record) {
+    return { id: record.id, path: record.file_path };
+  }
 }
 
 async function fetchTable(tableName) {
@@ -26,8 +111,8 @@ async function fetchTable(tableName) {
         'Content-Type': 'application/json'
       }
     });
-    const json = await res.json()
-    return normalizeArray(json.records);
+
+    return transformAirtableResponse(tableName, (await res.json()).records);
   } catch (error) {
     throw Error(error);
   }
@@ -38,4 +123,66 @@ function normalizeArray(items) {
   let result = {};
   items.forEach(item => { result[item.id] = item; });
   return result;
+}
+
+function isTranslated(key) {
+  return key.lastIndexOf('(en)') > -1 || key.lastIndexOf('(fr)')  > -1;
+}
+
+function getLanguage(key) {
+  return key.substring(
+    key.lastIndexOf("(") + 1, 
+    key.lastIndexOf(")")
+  );
+}
+
+function removeLanguageKey(key) {
+  const lang = getLanguage(key);
+  return key.replace(`_(${lang})`, '');
+}
+
+function transformAirtableResponse(tableName, records) {
+  return records.map(item => {
+    let result = {};
+    result.id = item.id;
+    result.date = item.createdTime;
+    result.from_table = tableName.toLowerCase();
+
+    Object.keys(item.fields).forEach(key => {
+      result[key.toLowerCase().replace(/\s/g, '_')] = item.fields[key];
+    });
+
+    let title = '';
+    let basedir = '';
+    switch (result.from_table) {
+      case 'talk':
+        title = result['title_(en)']
+        basedir = '/talks/';
+        break;
+      case 'speaker':
+        title = result.name;
+        basedir = '/speakers/'; 
+        break;
+      case 'event': 
+        title = result.name;
+        basedir = '/event/'; 
+        break;
+      case 'chapter': 
+        title = result.name;
+        basedir = '/chapters/'; 
+        break;
+      default:
+        title = '';
+        basedir = ''; 
+    }
+
+    if (!title) {
+      return null;
+    }
+
+    const filename = title.replace(/\s/g, '-').toLowerCase();
+    result.file_path = `${basedir}${filename}`;
+
+    return result;
+  }).filter(item => item);
 }
